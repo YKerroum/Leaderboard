7fdeaaa94b19ead95b70fb0413cd1de9fa3daad3
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2022-11-15 15:42:29 +0100

    fix: fix linters errors

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 export default class Data {
-  constructor(){
-  this.scores=[];
+  constructor() {
+    this.scores = [];
   }
 
-  getData=() =>{
-  this.scores=  localStorage.getItem('scores') ? JSON.parse(localStorage.getItem('scores')) : [];
+  getData=() => {
+    this.scores = localStorage.getItem('scores') ? JSON.parse(localStorage.getItem('scores')) : [];
   }
 
-  setData=(score) =>{
-  this.scores.push(score);
-  localStorage.setItem('scores', JSON.stringify(this.scores));
-  } 
+  setData=(score) => {
+    this.scores.push(score);
+    localStorage.setItem('scores', JSON.stringify(this.scores));
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,32 +1,28 @@
 import './index.css';
-import Data from './data';
+import Data from './data.js';
 
-const formElement=document.querySelector('#add-form');
-const recentElement=document.querySelector('#recent-content');
+const formElement = document.querySelector('#add-form');
+const recentElement = document.querySelector('#recent-content');
 
-const data=  new Data();
-const fillList= () => {
-data.getData();
-recentElement.innerHTML='';
-data.scores.forEach((element,index) => {
-recentElement.innerHTML += `
-<li id="${index}li" class="forms ${index%2? 'greyList' : ''}">${element.name} : ${element.score}</li>
-`
-})
-
-}
+const data = new Data();
+const fillList = () => {
+  data.getData();
+  recentElement.innerHTML = '';
+  data.scores.forEach((element, index) => {
+    recentElement.innerHTML += `
+<li id="${index}li" class="forms ${index % 2 ? 'greyList' : ''}">${element.name} : ${element.score}</li>
+`;
+  });
+};
 
 window.onload = () => {
-
-fillList();
-formElement.addEventListener("submit", (e) => {
-data.setData({name:e.target.querySelector('#name').value ,score:e.target.querySelector('#score').value});
-console.log(data);
-fillList();
-});
-
-document.querySelector("#refresh").addEventListener("click",()=>{
   fillList();
-});
+  formElement.addEventListener('submit', (e) => {
+    data.setData({ name: e.target.querySelector('#name').value, score: e.target.querySelector('#score').value });
+    fillList();
+  });
 
-}
+  document.querySelector('#refresh').addEventListener('click', () => {
+    fillList();
+  });
+};
