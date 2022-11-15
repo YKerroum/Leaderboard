57ecdd0a7b8213d83b418d5acb2c95deb2b2068a
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2022-11-15 13:20:32 +0100

    feat: add listeners to submit and refresh buttons and filllist on the load

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import './index.css';
+import Scores from './scores'
+import Data from './data';
 
-window.onload = () => {
+const formElement=document.querySelector('#add-form');
+const recentElement=document.querySelector('#recent-content');
 
 
+const fillList= (datas) => {
+recentElement.innerHTML='';
+datas.forEach((data,index) => {
+recentElement.innerHTML += `
+<li id="${index}li" class="${index%2? 'greyList' : ''}">${data.name} : ${data.score}</li>
+`
+})
 
 }
 
+window.onload = () => {
+const data= Data.getData;
+const scores=new Scores(data);
 
+formElement.addEventListener('submit', (e) => {
+Data.setData(scores.addScore({name:e.target.querySelector('#name').value ,score:e.target.querySelector('#score').value}));
+fillList(scores);
+})
+
+document.querySelector("#refresh").addEventListener("click",()=>{
+  const data= Data.getData;
+  fillList(data);
+})
+
+}
